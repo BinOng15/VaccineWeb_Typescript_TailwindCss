471b3be594beccdd076fd2021dac87ad453d7952
truongduy2804 <duy08201@gmail.com>
2025-02-17 10:17:18 +0700

    payment

--- src/pages/Customer/Payment.tsx
+++ src/pages/Customer/Payment.tsx
@@ -12,7 +12,7 @@ const PaymentMethod: React.FC = () => {
   const handleHome = (): void => {
     navigate('/'); // This will navigate to the home page
   };
-
+//
 
   return (
     <div className="max-w-4xl mx-auto p-6   rounded-xl">
