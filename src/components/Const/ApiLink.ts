433d2fa1db5c1dac16e23054e7ef6db8aeb949d3
Dư Trần Vĩnh Hưng <135632758+BinOng15@users.noreply.github.com>
2025-03-24 09:11:32 +0700

    Update ApiLink.ts

--- src/components/Const/ApiLink.ts
+++ src/components/Const/ApiLink.ts
@@ -1,3 +1,4 @@
 export const APILink =
   //"https://pedivaxapi-ddfzcbhfd0dwbpfa.southeastasia-01.azurewebsites.net";
-  "https://localhost:7105";
+  //"https://localhost:7105";
+  "http://marvinle-001-site1.ntempurl.com";
